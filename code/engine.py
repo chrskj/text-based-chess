4aead079d8cdb6212df62673e56e944bd888bb8f
Christofer Gilje Skjæveland <christofer@dhcp-10-20-54-63.wlan.ntnu.no>
2015-11-13 08:40:22 +0100

    Ting blei endra kan du sei

--- code/engine.py
+++ code/engine.py
@@ -4,30 +4,40 @@ from user import *
 
 
 class Engine(object):
-
     def __init__(self):
         self.turn = 0
-        # board not used? Tør ikkje sletta an
         board = GameBoard()
         self.player_white = User('W')
         self.player_black = User('B')
         while True:
-            turn += 1
-            self.update()
-            if turn >= 10:
+            self.turn += 1
+            print(board.console_board(self.update(board)))
+            if self.turn >= 10:
                 break
 
-    def update(self):
+    def update(self, board):
+        """Spør bruker om flytting av brikker og oppdaterer brettet."""
         print(self.turn)
+
         if self.turn % 2:
             print('White\'s turn!')
             white_choice = self.player_white.movement_input()
-            return [white_choice[0], white_choice[1],
-                    white_choice[2], white_choice[3]]
+            choice_list = [int(white_choice[0]), int(white_choice[1]), int(white_choice[2]), int(white_choice[3])]
         else:
             print('Black\'s turn!')
             black_choice = self.player_black.movement_input()
-            return [black_choice[0], black_choice[1],
-                    black_choice[2], black_choice[3]]
+            choice_list = [int(black_choice[0]), int(black_choice[1]), int(black_choice[2]), int(black_choice[3])]
+
+        brikke_valg = board.pos[choice_list[0]][choice_list[1]]
+        print(brikke_valg)
+        brikke_plassering = board.pos[choice_list[2]][choice_list[3]]
+        print(brikke_plassering)
+        brikke_plassering = brikke_valg
+        brikke_valg = '.'
+        print(brikke_valg)
+        print(brikke_plassering)
+        print(board.pos)
+
+
 
 Engine()
--- code/gameboard.py
+++ code/gameboard.py
@@ -5,9 +5,9 @@ from gamepieces import *
 
 class GameBoard(object):
     def __init__(self):
-        pos = self.game_board()
-        self.piece_setup(pos)
-        self.console_board(pos)
+        self.pos = self.game_board()
+        self.piece_setup(self.pos)
+        self.console_board(self.pos)
 
     def game_board(self):
         """Creates the board which will be used."""
@@ -24,31 +24,31 @@ class GameBoard(object):
             teller = -1
             if x == 1 or x == 6:
                 for y in range(8):
-                    pos[6][y] = 'P'
-                    pos[1][y] = 'p'
+                    self.pos[6][y] = 'P'
+                    self.pos[1][y] = 'p'
             else:
                 for y in range(8):
                     teller += 1
-                    pos[x][y] = pieces[teller]
-                    pos[0][y] = pos[0][y].lower()
+                    self.pos[x][y] = pieces[teller]
+                    self.pos[0][y] = self.pos[0][y].lower()
 
     def console_board(self, pos):
         """Visualizes the game board in the console."""
 
-        print(' %s %s %s %s %s %s %s %s\n' % (pos[0][0], pos[0][1], pos[0][2], pos[0][3], pos[0][4], pos[0][5],
-                                              pos[0][6], pos[0][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[1][0], pos[1][1], pos[1][2], pos[1][3], pos[1][4], pos[1][5],
-                                             pos[1][6], pos[1][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[2][0], pos[2][1], pos[2][2], pos[2][3], pos[2][4], pos[2][5],
-                                             pos[2][6], pos[2][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[3][0], pos[3][1], pos[3][2], pos[3][3], pos[3][4], pos[3][5],
-                                             pos[3][6], pos[3][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[4][0], pos[4][1], pos[4][2], pos[4][3], pos[4][4], pos[4][5],
-                                             pos[4][6], pos[4][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[5][0], pos[5][1], pos[5][2], pos[5][3], pos[5][4], pos[5][5],
-                                             pos[5][6], pos[5][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[6][0], pos[6][1], pos[6][2], pos[6][3], pos[6][4], pos[6][5],
-                                             pos[6][6], pos[6][7]),
-              '%s %s %s %s %s %s %s %s\n' % (pos[7][0], pos[7][1], pos[7][2], pos[7][3], pos[7][4], pos[7][5],
-                                             pos[7][6], pos[7][7])
+        print(' %s %s %s %s %s %s %s %s\n' % (self.pos[0][0], self.pos[0][1], self.pos[0][2], self.pos[0][3], self.pos[0][4], self.pos[0][5],
+                                              self.pos[0][6], self.pos[0][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[1][0], self.pos[1][1], self.pos[1][2], self.pos[1][3], self.pos[1][4], self.pos[1][5],
+                                             self.pos[1][6], self.pos[1][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[2][0], self.pos[2][1], self.pos[2][2], self.pos[2][3], self.pos[2][4], self.pos[2][5],
+                                             self.pos[2][6], self.pos[2][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[3][0], self.pos[3][1], self.pos[3][2], self.pos[3][3], self.pos[3][4], self.pos[3][5],
+                                             self.pos[3][6], self.pos[3][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[4][0], self.pos[4][1], self.pos[4][2], self.pos[4][3], self.pos[4][4], self.pos[4][5],
+                                             self.pos[4][6], self.pos[4][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[5][0], self.pos[5][1], self.pos[5][2], self.pos[5][3], self.pos[5][4], self.pos[5][5],
+                                             self.pos[5][6], self.pos[5][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[6][0], self.pos[6][1], self.pos[6][2], self.pos[6][3], self.pos[6][4], self.pos[6][5],
+                                             self.pos[6][6], self.pos[6][7]),
+              '%s %s %s %s %s %s %s %s\n' % (self.pos[7][0], self.pos[7][1], self.pos[7][2], self.pos[7][3], self.pos[7][4], self.pos[7][5],
+                                             self.pos[7][6], self.pos[7][7])
               )
