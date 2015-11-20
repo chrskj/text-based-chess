22f4bafe59f8b23e5278ff54cd198ef7c80091db
Malann <malann.sjakk@gmail.com>
2015-11-20 03:18:20 +0100

    Tkinter e noge dritt

--- code/chessboard.py
+++ code/chessboard.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from pathlib import Path
+from PIL import Image, ImageTk
+# from pathlib import Path
 
 class ChessBoard(tk.Frame):
 
@@ -12,10 +13,15 @@ class ChessBoard(tk.Frame):
         size = 70
         width = self.height = 70*8
 
+        self.images = [ [None for x in range(8)] for y in range(8) ]
+        self.images.append(tk.PhotoImage(file='resources/b_b.png'))
+
         tk.Frame.__init__(self, parent)
         self.canvas = tk.Canvas(self, borderwidth = 0, highlightthickness = 0,
                                 width = width, height = self.height,
                                 background = 'blue')
+
+
         self.canvas.pack(side = 'top', fill = 'both', expand = True)
 
         fill = fill1 = 'green'
@@ -28,10 +34,39 @@ class ChessBoard(tk.Frame):
                 self.canvas.create_rectangle(j*size, i*size, (j+1)*size,
                                              (i+1)*size, fill=fill )
 
-def main():
-    root = tk.Tk()
-    brett = ChessBoard(root)
-    brett.pack(side = 'top', fill = 'both', expand = True)
-    root.mainloop()
 
-main()
+    def place_piece(self, x, y, piece_letter, piece_color):
+        letr = piece_letter.lower()
+        colr = piece_color[0].lower()
+
+        photo = tk.PhotoImage(file=(letr + '_' + colr + '.png'))
+
+        self.images[x][y] = photo
+
+
+    def remove_piece(self, x, y):
+        self.images[x][y] = None
+
+
+    def visualize(self):
+        for i in range(8):
+            rad = self.images[i]
+            for j in range(8):
+                rute = rad[j]
+                if rute:
+                    self.canvas.create_image(
+                        (i*70) + 5,
+                        (j * 70 ) + 5,
+                        file = rute
+                    )
+
+
+root = tk.Tk()
+brett = ChessBoard(root)
+brett.pack(side = 'top', fill = 'both', expand = True)
+root.mainloop()
+
+print('213123')
+
+brett.place_piece(3, 4, 'b','b')
+
